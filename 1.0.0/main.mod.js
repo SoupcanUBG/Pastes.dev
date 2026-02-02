2890f817cc3bd290bc54b4a555f19f1151ee14db
Toby Lopez <216323458+SoupcanUBG@users.noreply.github.com>
2026-02-02 01:29:56 +0000

    loL

---
+++ 1.0.0/main.mod.js
@@ -0,0 +1,25 @@
+import { PolyMod } from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.2/PolyModLoader.js";
+class PastesDevImport extends PolyMod {
+   
+init = (pml) => {
+
+
+//pml.registerClassMixin(hx.prototype, fromExportString, MixinType.INSERT, firstToken: fromExportString, secondTokenOrFunction: fromExportString | [FUNCTION], functionOptional: [FUNCTION]);
+
+
+
+
+}
+
+
+
+}
+//class PastesDevExport extends PolyMod {
+//  init = (pml) => {
+//  pml.registerClassMixin();
+
+}
+
+
+export let polyMod = new PastesDevImport();
+
