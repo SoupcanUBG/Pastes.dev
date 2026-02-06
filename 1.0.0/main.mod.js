4e0e4a5e9f889682ed23e709f4568d32a0eb2606
Toby Lopez <216323458+SoupcanUBG@users.noreply.github.com>
2026-02-06 19:18:32 +0000

    Go Back To Original

--- 1.0.0/main.mod.js
+++ 1.0.0/main.mod.js
@@ -1,47 +1,20 @@
 import { PolyMod, MixinType } from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.2/PolyModLoader.js";
+class PastesDevImport extends PolyMod {
+init = (pml) => {
 
-class PolyPastes extends PolyMod {
-    loadFromPaste = (link) => {
-        let regex = /https:\/\/pastes\.dev\/[A-Za-z0-9]+/i;
 
-        if (regex.test(link)) {
-            const fromExportString = this.pml.getFromPolyTrack("hx.fromExportString");
+pml.registerClassMixin(hx, fromExportString, MixinType.INSERT,  "const s = XA(e);", "const PastesDevExport = PastesDev(e)" );
 
-            console.log(link.split("dev/")[1]);
 
-            const i = new XMLHttpRequest();
-            i.open(
-                "GET",
-                `https://api.pastes.dev/${link.split("dev/")[1]}`,
-                false
-            );
-            i.send();
-
-            return fromExportString(i.responseText);
-        }
-
-        return null;
-    };
-
-    init = (pml) => {
-        this.pml = pml;
-
-        pml.getFromPolyTrack(
-            `window.__polypastesmod = ActivePolyModLoader.getMod("${this.modID}")`
-        );
-
-        pml.registerClassMixin(
-            "hx",
-            "fromExportString",
-            MixinType.INSERT,
-            `
-            const s = XA(e);
-            if (s == null) {
-                return window.__polypastesmod.loadFromPaste(e);
-            }
-            `
-        );
-    };
 }
 
-export let polyMod = new PolyPastes();
+
+
+}
+//class PastesDevExport extends PolyMod {
+//  init = (pml) => {
+  //pml.registerClassMixin();
+
+//}
+  export let polyMod = new PastesDevImport();
+
