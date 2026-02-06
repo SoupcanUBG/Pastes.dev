cb98f509518cde774c0d693ca16a26516bff417b
Toby Lopez <216323458+SoupcanUBG@users.noreply.github.com>
2026-02-06 19:10:34 +0000

    lol

--- 1.0.0/main.mod.js
+++ 1.0.0/main.mod.js
@@ -1,25 +1,47 @@
-
-import { PolyMod, MixinType} from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.2/PolyModLoader.js";
+import { PolyMod, MixinType } from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.2/PolyModLoader.js";
 
 class PolyPastes extends PolyMod {
-  loadFromPaste(link) {
-	const regex = /https:\/\/pastes\.dev\/[A-Za-z0-9]+/i;
-	if(regex.test(link)) {
-	  const fromExportString = this.pml.getFromPolyTrack("hx.fromExportString");
-	  console.log(link.split("dev/")[1]);
-	  const i = new XMLHttpRequest();
-	  i.open("GET", `https://api.pastes.dev/${link.split("dev/")[1]}`, false);
-	  i.send();
-	  return fromExportString(i.responseText);
-	}
-	return null;
-  }
+    loadFromPaste = (link) => {
+        let regex = /https:\/\/pastes\.dev\/[A-Za-z0-9]+/i;
 
-  init = (pml) => {
-	this.pml = pml;
-	pml.getFromPolyTrack(`window._polypastesmod = ActivePolyModLoader.getMod("${this.modID}")`);
-	pml.registerClassMixin("hx", "fromExportString", MixinType.INSERT, `const s = XA(e);`, `if(s == null) { return window.__polypastesmod.loadFromPaste(e); }`);
-  }
+        if (regex.test(link)) {
+            const fromExportString = this.pml.getFromPolyTrack("hx.fromExportString");
+
+            console.log(link.split("dev/")[1]);
+
+            const i = new XMLHttpRequest();
+            i.open(
+                "GET",
+                `https://api.pastes.dev/${link.split("dev/")[1]}`,
+                false
+            );
+            i.send();
+
+            return fromExportString(i.responseText);
+        }
+
+        return null;
+    };
+
+    init = (pml) => {
+        this.pml = pml;
+
+        pml.getFromPolyTrack(
+            `window.__polypastesmod = ActivePolyModLoader.getMod("${this.modID}")`
+        );
+
+        pml.registerClassMixin(
+            "hx",
+            "fromExportString",
+            MixinType.INSERT,
+            `
+            const s = XA(e);
+            if (s == null) {
+                return window.__polypastesmod.loadFromPaste(e);
+            }
+            `
+        );
+    };
 }
 
 export let polyMod = new PolyPastes();
